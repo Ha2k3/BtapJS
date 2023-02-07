46cce2a5c9179fbec796593b00851bcadcdfb21e
Vothithuyha <116275455+Ha2k3@users.noreply.github.com>
2023-02-07 15:54:00 +0700

    f

---
+++ Array/bai3product.js
@@ -0,0 +1,50 @@
+var product = [
+    {
+        id: 1,
+        name: "IP11Pro",
+        img: "https://hc.com.vn/i/ecommerce/media/GS.008504_FEATURE_93851.jpg",
+        price: 3190000
+    },
+    {
+        id: 2,
+        name: "SONNY XZ2",
+        img: "https://cdn.tgdd.vn/Products/Images/42/146014/sony-xperia-xz2-1-600x600.jpg",
+        price: 3190000
+    },
+    {
+        id: 3,
+        name: "SamSunga02",
+        img: "https://cdn.tgdd.vn/Products/Images/42/228999/samsung-galaxy-a02-den-600x600-200x200.jpg",
+        price: 3190000
+    },
+    {
+        id: 4,
+        name: "SamSunga02",
+        img:"https://cdn.tgdd.vn/Products/Images/42/228999/samsung-galaxy-a02-den-600x600-200x200.jpg",
+        price: 3190000
+    },
+    {
+        id: 5,
+        name: "SamSunga02",
+        img:"https://cdn.tgdd.vn/Products/Images/42/228999/samsung-galaxy-a02-den-600x600-200x200.jpg",
+        price: 3190000
+    },
+    {
+        id: 6,
+        name: "SamSunga02",
+        img:"https://cdn.tgdd.vn/Products/Images/42/228999/samsung-galaxy-a02-den-600x600-200x200.jpg",
+        price: 3190000
+    },
+    {
+        id: 7,
+        name: "SONNY XZ2",
+        img: "https://cdn.tgdd.vn/Products/Images/42/146014/sony-xperia-xz2-1-600x600.jpg",
+        price: 3190000
+    },
+    {
+        id: 8,
+        name: "SamSunga02",
+        img:"https://cdn.tgdd.vn/Products/Images/42/228999/samsung-galaxy-a02-den-600x600-200x200.jpg",
+        price: 3190000
+    },
+]
